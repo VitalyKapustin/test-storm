cd4204a0cc2c2b10cb111a241015964d3cb340ae
v.kapustin <breakcool@gmail.com>
2015-08-06 12:31:23 +0300

    Merge branch 'add-couchbase-repository' of https://github.com/VitalyKapustin/test-storm into add-couchbase-repository
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- src/main/java/com/insart/titanium/configuration/ApplicationContextProvider.java
+++ src/main/java/com/insart/titanium/configuration/ApplicationContextProvider.java
@@ -10,33 +10,35 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  * Created by v.kapustin on 8/3/15.
  */
 public class ApplicationContextProvider {
-    private transient ConfigurableApplicationContext springContext;    
+
     private static transient volatile ApplicationContextProvider instance;
 
-    private ApplicationContextProvider(){
-	springContext = new AnnotationConfigApplicationContext();
+    private transient ConfigurableApplicationContext springContext;
+
+    private ApplicationContextProvider() {
+        springContext = new AnnotationConfigApplicationContext(ApplicationConfig.class);
     }
-    
-    public static ApplicationContextProvider getInstance(){
-	if(instance == null){
-	    synchronized (ApplicationContextProvider.class) {
-		if(instance == null){
-		    instance = new ApplicationContextProvider();
-		}
-	    }
-	}
-	return instance;
+
+    public static ApplicationContextProvider getInstance() {
+        if (instance == null) {
+            synchronized (ApplicationContextProvider.class) {
+                if (instance == null) {
+                    instance = new ApplicationContextProvider();
+                }
+            }
+        }
+        return instance;
     }
-    
+
     public ConfigurableApplicationContext getApplicationContext() {
-	return springContext;
+        return springContext;
     }
 
     public <T> T getBean(Class<T> clazz) {
-	return springContext.getBean(clazz);
+        return springContext.getBean(clazz);
     }
-    
-    public static void autowireObject(Object object){
-	ApplicationContextProvider.getInstance().getApplicationContext().getAutowireCapableBeanFactory().autowireBean(object);
+
+    public static void autowireObject(Object object) {
+        getInstance().getApplicationContext().getAutowireCapableBeanFactory().autowireBean(object);
     }
 }
